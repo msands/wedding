eca892e244c31632f9f6947928e393e15a08aeb8
Marcel Sands <marcel.sands84@gmail.com>
2013-10-11 17:36:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedding::Application.config.secret_key_base = '885d4c4f1c61ed0df6c5f8b3bb5c0d372d17ee15de2b764ed7caed6aafe9f34c1f1a431775c0120f1a89c76174c834ed89b020f1b12e74cc8bffd47c1c71d2ef'
