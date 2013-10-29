71e90b7e3394c49e23f5771d53a3847f3d9266e2
Marcel Sands <marcel.sands84@gmail.com>
2013-10-28 23:31:07 -0400

    generated users in Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Wedding::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" ## creates about_path
-  get "party" => "pages#party" ## creates about_path
+  get "party" => "pages#party" ## creates party_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
