caaa6cc408ecf49a2e201f5f7db40edfaee40937
Marcel Sands <marcel.sands84@gmail.com>
2014-01-29 18:03:03 -0500

    rename carousel.css and complete pins view

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :image, :styles => { :medium => "600x1200", :thumb => "600x600" }
+  has_attached_file :image, :styles => { :medium => "600x800", :thumb => "300x225" }
   
   validates :image, presence: true
   validates :description, presence: true
